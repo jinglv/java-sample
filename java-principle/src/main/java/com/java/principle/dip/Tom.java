bf53cce8b0025b19fbe1c346766e9de41a7a6863
jinglv <lvjing0705@163.com>
2020-07-05 21:04:53 +0800

    [add]栈学习笔记

---
+++ java-principle/src/main/java/com/java/principle/dip/Tom.java
@@ -0,0 +1,15 @@
+package com.java.principle.dip;
+
+/**
+ * @author jinglv
+ * @date 2020-07-04 2:57 下午
+ */
+public class Tom {
+    public void studyJavaCourse(){
+        System.out.println("Tome在学习java的课程");
+    }
+
+    public void studyPythonCourse(){
+        System.out.println("Tome在学习python的课程");
+    }
+}
